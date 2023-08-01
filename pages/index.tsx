8e9aef70aedbf47bc8fa18c9c68ab76299b1e07a
Dan Keyburn <dan.keyburn@gmail.com>
2023-08-01 12:58:13 -0500

    quote generator draft working, no API connection yet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+import React, { useState } from "react";
+
+// Components
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  FooterCon,
+  FooterLink,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+  GradientBackgroundCon,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorSubTitle,
+  QuoteGeneratorTitle,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+// Assets
+import Clouds1 from "../assets/cloud-and-thunder.png";
+import Clouds2 from "../assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -12,8 +33,53 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/*Background*/}
-      <GradientBackgroundCon></GradientBackgroundCon>
+
+      {/* Background */}
+      <GradientBackgroundCon>
+        {/* Quote Generator Modal Pop-Up*/}
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>Daily Quote Generator</QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Get your inspiration here with a quote provided by{" "}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes Api
+              </FooterLink>
+            </QuoteGeneratorSubTitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Generate a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1" />
+
+        <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground1" />
+
+        {/* Footer */}
+        <FooterCon>
+          <>Quotes Generated: {numberOfQuotes}</>
+          <br />
+          Developed by{" "}
+          <FooterLink
+            href="https://dankeyburn.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dan Keyburn
+          </FooterLink>
+        </FooterCon>
+      </GradientBackgroundCon>
     </>
   );
 }
